f103c42542021faf054e88beb486ca4c1afbaacd
Kawase-Hiroki <hiro.kms0829@gmail.com>
2025-07-18 18:37:15 +0900

    correction

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,13 @@
 document.getElementById("show").addEventListener("click", () => {
-    chrome.storage.local.get(null, (result) => {
-      document.getElementById("output").textContent = JSON.stringify(result, null, 2);
-    });
+  chrome.storage.local.get(null, (result) => {
+    document.getElementById("output").textContent = JSON.stringify(result, null, 2);
   });
+});
   
-  document.getElementById("clear").addEventListener("click", () => {
-    chrome.storage.local.clear(() => {
-      alert("保存されたデータをすべて削除しました！");
-      document.getElementById("output").textContent = "";
-    });
+document.getElementById("clear").addEventListener("click", () => {
+  chrome.storage.local.clear(() => {
+    alert("保存されたデータをすべて削除しました！");
+    document.getElementById("output").textContent = "";
   });
+});
   
